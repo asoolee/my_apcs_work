6b87a281b94f3a5465039db25b16ef6516240dcc
shugenesis <adisoominlee@gmail.com>
2025-10-03 11:52:20 -0700

    assignment 3

--- unit3/CoinFlip.java
+++ unit3/CoinFlip.java
@@ -1,11 +1,13 @@
 public class CoinFlip{
-	public static String coinFlip(String coin){
+	public static String coinFlip(){
 		
-		double flip = (int)Math.random()*10;
+		double flip = (int)(Math.random()*10);
+		
+		String coin = "";
 		
 		if(flip < 5)
 			coin = "Heads";
-		 else if (flip > 5)
+		 else if (flip >= 5)
 			coin = "Tails";
 		
 		return coin;
@@ -14,7 +16,7 @@ public class CoinFlip{
 	public static void main(String[] args){
 		
 		for(int i = 0; i < 20; i++){
-			System.out.println(coin);
+			System.out.println(coinFlip());
 			}
 		
 		}
