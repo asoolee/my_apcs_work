ab1f42b3c26d5b4eddad4067b413d4ee0bab83fa
shugenesis <adisoominlee@gmail.com>
2026-01-06 13:40:40 -0800

    m

--- Project/Arrival.java
+++ Project/Arrival.java
@@ -3,30 +3,51 @@ import java.util.*;
 
 public class Arrival
 {
-	public Arrival(PApplet p, PVector posIn)
+	public Arrival(PApplet p, PVector posIn, PVector targetIn)
 	{
 		this.p = p;
 		position = posIn;
-		velocity = new PVector(p.random(-2,2),p.random(-2,2));
+		target = targetIn;
+		velocity = new PVector(p.random(-1,2),p.random(-1,2));
 		radius = 1;
 		c = p.color(0);
 	}
 	
 	public void display()
 	{
+		p.noStroke();
 		p.fill(c);
 		p.ellipse(position.x,position.y,radius*2, radius*2);
 		
+		// now change to target
+		
+		//for position isnt at target, add acc
+		PVector acc = PVector.sub(target, position);
+		
+		
+		if(acc.mag() > 0.1)
+		{
+			acc.mult(.005f);
+		}
+	
+		velocity.add(acc);
 		position.add(velocity);
 		
-		PVector acc = new PVector(p.mouseX - position.x, p.mouseY - position.y);
-		acc.mult(.001f);
-		velocity.add(acc);
+		if(p.dist(target.x, target.y, position.x, position.y) < 100)
+		{
+			velocity.mult(0.8f);
+			if(velocity.mag() < 3)
+			velocity.setMag(3);
+		}
+		
 	}
+
 	
 	private PApplet p; 
     private PVector position;
     private PVector velocity;
+    private PVector target;
     private float radius;
     private int c;
+    private int n = 0;
 }
--- Project/ArrivalMain.java
+++ Project/ArrivalMain.java
@@ -6,33 +6,68 @@ public class ArrivalMain extends PApplet
 	public void settings()
 	{
 		fullScreen();
+		
 	}
 	
 	public void setup()
 	{
 		arrivals = new ArrayList<Arrival>();
-		int angle = 0;
+		 //int angle = 0;
 		
-		for(int i = 0; i < 1500; i++)
-		{
-			arrivals.add(new Arrival(this, new PVector(width/2 + cos(angle)*150, height/2 + sin(angle)*150)));
-			angle++;
-		}
+		//for(int i = 0; i < 1500; i++)
+		//{
+		//arrivals.add(new Arrival(this, new PVector(width/2 + cos(angle)*150, height/2 + sin(angle)*150)));
+		//arrivals.add(new Arrival(this, new PVector(random(width/3,width/2), random(height/3,height/2))));
+		//angle++;
+		//}
+		
+		cx = width/2;
+		cy = height/2;
+		r = width/6;
+		
+		background(255);
+		
 	}
 	
 	public void draw()
 	{
-		background(255);
+		fill(255,30);
+		rect(0,0, width, height);
+	// should change to width height 
+	
+		if(spawn)
+		{
+			n++;
+			float t = random(0, TWO_PI);
+			arrivals.add(new Arrival(this, new PVector(mouseX, mouseY), new PVector(cx+r*cos(t),cy+r*sin(t))));
+		}
 		for(Arrival a: arrivals)
 		a.display();
 	}
 	
-	private ArrayList<Arrival> arrivals;
-
+	public void mousePressed()
+	{
+		spawn = true;
+	}
+	
+	public void mouseReleased()
+	{
+		spawn = false;
+	}
+	
+	
     public static void main(String[] args)
     {
         PApplet.main("ArrivalMain");
     }
+    
+  
+    private ArrayList<Arrival> arrivals;
+	private boolean spawn = false;
+	private float cx, cy;
+	private float r;
+	private float N = 1400;
+	private float n = 0;
     
 }
 
