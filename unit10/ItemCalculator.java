342b6d994593df994735bd45080d04a0134c607f
shugenesis <adisoominlee@gmail.com>
2026-01-14 14:45:14 -0800

    un10

--- unit10/ItemCalculator.java
+++ unit10/ItemCalculator.java
@@ -33,15 +33,27 @@ public class ItemCalculator
 	
 	public static void main(String[] args)
 	{
-	ArrayList<Item> items = new ArrayList<Item>();
+		float expectedAverage = 30f;
+		float expectedHighest = 50f;
+		
+		ArrayList<Item> items = new ArrayList<Item>();
 	
-	items.add(new Item("Hat", 30.00f));
-	items.add(new Item("Shoes", 50.00f));
-	items.add(new Item("Shirt", 10.00f));
+		items.add(new Item("Hat", 30.00f));
+		items.add(new Item("Shoes", 50.00f));
+		items.add(new Item("Shirt", 10.00f));
 	
-	printItems(items);
-	System.out.println("average price " + getAverage(items));
-	System.out.println("highest price " + getHighest(items));
+		printItems(items);
+				
+		System.out.println("average price " + getAverage(items));
+		if(expectedAverage == getAverage(items))
+			System.out.println("yay");
+		else
+			System.out.println("no");
+		System.out.println("highest price " + getHighest(items));
+		if(expectedHighest == getHighest(items))
+			System.out.println("yay");
+		else
+			System.out.println("no");
 	}
 }
 
--- unit10/PersonStats.java
+++ unit10/PersonStats.java
@@ -2,25 +2,71 @@ import java.util.*;
 
 public class PersonStats
 {
-	public static int getAverage(ArrayList<Person> persons)
+	public static float getAverage(ArrayList<Person> persons)
 		{
 			int average = 0;
 		
 			for(Person p: persons)
-				average += p.getAge();
+				average += p.getPets();
 			
 			return average / persons.size();
 		}
 	
-	public static string mostPets(ArrayList<Person> persons)
+	public static String mostPets(ArrayList<Person> persons)
 		{
+			Person highest = null;
+			int highestPets = 0;
+			
+			for(int i = 0; i < persons.size(); i++)
+			{
+				if(persons.get(i).getPets() > highestPets)
+				{
+					highestPets = persons.get(i).getPets();
+					highest = persons.get(i);
+				} 
+			}
+			
+			return highest.getName();
+			
+		}
+		
+	public static ArrayList<String> more2Pets(ArrayList<Person> persons)
+		{
+			ArrayList<String> result = new  ArrayList<String>();
 			
 			for(Person p: persons)
 			{
-				Person highest = p;
-				// wip
-				if(
+				if( p.getPets() >= 2)
+				{
+					result.add(p.getName());
+				}
 			}
 			
+			return result;
 		}
+	
+	public static void main(String[] args)
+	{
+		int expectedAverage = 2;
+		String expectedPets = "Sarah";
+		
+		ArrayList<Person> test = new ArrayList<Person>();
+		test.add(new Person("Sarah", 5));
+		test.add(new Person("Jazzy", 1));
+		test.add(new Person("Sofia", 3));
+		test.add(new Person("Dad", 4));
+		test.add(new Person("Me", 1));
+		
+		System.out.println("Average pets: " + getAverage(test));
+		if(getAverage(test) == expectedAverage)
+			System.out.println("yay");
+		else
+			System.out.println("no");
+		System.out.println("Most pets: " + mostPets(test));
+		if(mostPets(test) == expectedPets)
+			System.out.println("yay");
+		else
+			System.out.println("no");
+		System.out.println("At least two pets: " + more2Pets(test));
+	}
 }
