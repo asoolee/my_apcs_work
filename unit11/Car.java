1183029794e18af1fd44fed057123204404bb479
shugenesis <adisoominlee@gmail.com>
2026-01-27 12:32:23 -0800

    unit11 half

---
+++ unit11/Car.java
@@ -0,0 +1,17 @@
+public class Car implements Vehicle
+{
+	public String name()
+	{
+		return "Car";
+	}
+	
+	public int wheelCount()
+	{
+		return 4;
+	}
+	
+	public boolean isHumanPowered()
+	{
+		return false;
+	}
+}
